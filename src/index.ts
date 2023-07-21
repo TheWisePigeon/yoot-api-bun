29b367243a9225a6cfa2ac59e24c8b581e444ab6
TheWisePigeon <zozozozeph@gmail.com>
2023-07-21 23:23:26 +0000

    feat: auth middleware to validate requests based on api keys

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { Elysia } from "elysia";
+import { Elysia, Handler } from "elysia";
+import { HttpResponse, ApiKey } from "./utils";
+import { API_KEY_PREFIX } from "./config";
+import sql from "./db";
+import bcrypt from "bcrypt"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app.group(
+  '/v1', app => app
+    .state('api_key', {})
+    .onBeforeHandle(async ({ headers, store }) => {
+      try {
+        const api_key = headers["authorization"] ?? ""
+        if (api_key == "") {
+          return HttpResponse(401, "API key not found in authorization header")
+        }
+        const splitted_key = api_key.split("$")
+        if(splitted_key.length!==2){
+          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+        }
+        const [partial_id, key] = splitted_key
+        const db_id = `${API_KEY_PREFIX}_${partial_id}`
+        const [db_key] = await sql<ApiKey[]>` select * from api_key where id=${db_id}`
+        if(!db_key){
+          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+        }
+        const key_matches = await Bun.password.verify(key, db_key.key, "bcrypt")
+        if (!key_matches) {
+          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+        }
+        store.api_key = db_key
+      } catch (err) {
+        return HttpResponse(500)
+      }
+    })
+    .post('/entities', ({ store })=>{
+      return HttpResponse()
+    })
+)
+
+app.listen(3000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  ` Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
---
+++ src/utils/index.ts
@@ -0,0 +1,44 @@
+export function HttpResponse( status: number = 200, data?:string ) {
+  return new Response(data, { status })
+}
+
+export type ApiKey = {
+    id: string,
+    name: string,
+    project: string,
+    permissions: string,
+    key: string,
+    owner: string
+}
+
+export type Permission = {
+    create_permission: boolean,
+    write_permission: boolean,
+    delete_permission: boolean
+}
+
+export const Types = ["Text", "Number", "Boolean", "Image"]
+
+export function entity_data_is_valid(fields: string[], types: string[]) {
+    if (fields.length === 0 || types.length === 0) return {
+        status: false, message: "An entity can not have an empty schema"
+    }
+    if (
+        (new Set(fields)).size !== fields.length
+    ) return {
+        status: false, message: "An entity can not have two fields of the same name"
+    }
+    const invalid_type = types.find(field_type => !Types.includes(field_type))
+    if (invalid_type) return {
+        status: false, message: `Invalid or unsupported type ${invalid_type}`
+    }
+    return {
+        status: true, message: ""
+    }
+}
+
+export function is_base_64(data: string) {
+    const base64Regex = /^data:[a-z-]+\/[a-z-]+;base64,/;
+    return base64Regex.test(data);
+}
+
