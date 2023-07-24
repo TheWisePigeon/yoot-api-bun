8f204f629b8fdbd6a634a87d8779ce4c1a49b6ac
TheWisePigeon <zozozozeph@gmail.com>
2023-07-24 08:01:48 +0000

    feat: create entity

--- src/index.ts
+++ src/index.ts
@@ -13,21 +13,21 @@ app.group(
       try {
         const api_key = headers["authorization"] ?? ""
         if (api_key == "") {
-          return HttpResponse(401, "API key not found in authorization header")
+          return HttpResponse(401, "ERR_MISSING_KEY")
         }
         const splitted_key = api_key.split("$")
-        if(splitted_key.length!==2){
-          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+        if (splitted_key.length !== 2) {
+          return HttpResponse(401, "ERR_INVALID_KEY")
         }
         const [partial_id, key] = splitted_key
         const db_id = `${API_KEY_PREFIX}_${partial_id}`
         const [db_key] = await sql<ApiKey[]>` select * from api_key where id=${db_id}`
-        if(!db_key){
-          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+        if (!db_key) {
+          return HttpResponse(401, "ERR_INVALID_KEY")
         }
         const key_matches = await Bun.password.verify(key, db_key.key, "bcrypt")
         if (!key_matches) {
-          return HttpResponse(401, "Invalid API key. Contact support if you think this is an error")
+          return HttpResponse(401, "ERR_INVALID_KEY")
         }
         store.api_key = db_key
       } catch (err) {
@@ -37,7 +37,7 @@ app.group(
     .use(entities_v1)
 )
 
-app.listen(3000, async()=>{
+app.listen(3000, async () => {
   const version = await sql` select version() `
   console.log(version)
 })
--- src/v1/Entities.ts
+++ src/v1/Entities.ts
@@ -1,7 +1,8 @@
-import Elysia, { t } from "elysia";
-import { ApiKey } from "../utils";
+import Elysia from "elysia";
+import { ApiKey, Permission, entity_data_is_valid, TypeParse } from "../utils";
 import sql from "../db";
 import { HttpResponse } from "../utils";
+import { z } from "zod";
 
 const setup = (app: Elysia) => app
   .state('api_key', {} as ApiKey)
@@ -9,7 +10,7 @@ const setup = (app: Elysia) => app
 
 const entities_v1 = (app: Elysia) => app
   .use(setup)
-  .get('/entities', async ({ store:{ api_key }})=>{
+  .get('/entities', async ({ store: { api_key } }) => {
     try {
       const { project } = api_key
       const entities = await sql` select * from entity where project=${project}`
@@ -18,32 +19,51 @@ const entities_v1 = (app: Elysia) => app
         JSON.stringify({ entities })
       )
     } catch (err) {
-      console.log(err)
+      console.log(`Error while getting list of entities ${err}`)
       return HttpResponse(500)
     }
   })
-  .post('/entities', async ({ store, body, headers })=>{
+  .post('/entities', async ({ store, body, request }) => {
     try {
-      const content_type = (headers["content-type"]! as string).split(";")[0]
-      if(content_type!=="multipart/form-data") return HttpResponse(415, "ERR_BAD_CONTENT_TYPE")
-      const { name, schema } = body
-      if(name==="") return HttpResponse(400, "ERR_EMPTY_NAME")
-      let parsed_schema : Record<string, string>
-      try {
+      const content_type = (request.headers.get("Content-Type") as string).split(";")[0]
+      if(content_type!=="multipart/form-data"){
+        return HttpResponse(415, "ERR_CONT_TYPE")
+      }
+      if (TypeParse(body, z.object({ name: z.string(), schema: z.string() }))) {
+        const { api_key: { permissions, project } } = store
+        const parsed_permissions = JSON.parse(permissions) as Permission
+        if (!parsed_permissions.create_permission) {
+          return HttpResponse(403, "ERR_CREATE_PERMISSION")
+        }
+        const { name, schema } = body
+        const [potential_duplicate] = await sql` select name from entity where name=${name} and project=${project}`
+        if (potential_duplicate) {
+          return HttpResponse(409, "ERR_DUPLICATE")
+        }
+        if (name === "") return HttpResponse(400, "ERR_EMPTY_NAME")
+        let parsed_schema: Record<string, string>
         parsed_schema = JSON.parse(schema) as Record<string, string>
-        let fields = Object.entries(parsed_schema)
-      } catch (_) {
-        return HttpResponse(400, "ERR_BAD_SCHEMA")
+        let [fields, types] = Object.entries(parsed_schema)
+        const { status, message } = entity_data_is_valid(fields, types)
+        if (!status) return HttpResponse(400, message)
+        await sql`
+        insert into entity(name, project, schema)
+        values(${name}, ${""}, ${schema})
+      `
+        return HttpResponse(201)
+
+      } else {
+        return HttpResponse(400, "ERR_BAD_REQUEST")
       }
     } catch (err) {
+      if (err instanceof Error) {
+        if (err.name === "SyntaxError") {
+          return HttpResponse(400, "ERR_BAD_SCHEMA")
+        }
+      }
       console.log(`Error while creating entity ${err}`)
       return HttpResponse(500)
     }
-  }, {
-    body: t.Object({
-      name: t.String(),
-      schema: t.String()
-    })
   })
 
 export default entities_v1
